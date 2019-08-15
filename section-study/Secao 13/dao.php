8a19929fe0f84b3f2140dfcb80e6270686e28c86
Vicente Andrade <vincentandrad.e14@gmail.com>
2019-08-15 10:48:03 -0300

    Add new section 13 - PDO/DAO

---
+++ section-study/Secao 13/dao.php
@@ -0,0 +1,14 @@
+<?php
+
+# DAO(Data Acess Obeject): É a mistura entre POO & BD com DAO.
+
+# Camada Web: BD -> PHP -> FRONT-END <- , existe sempre comando no banco.
+
+# Criamos classes para mexer no banco de dados.
+
+# Programador Sênior: Cria as classes e ensina os demais programadores a como mexer com elas.
+# Programadores Junior/Pleno: Utiliza somente das classes ensinadas pelo o Sênior.
+
+# Acesso de Banco fica protegido.
+
+?>
